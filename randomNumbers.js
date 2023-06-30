0382bfe9314f6f2c86871d52c3d29ad40cd1925f
rdhafa <rdhafa77@gmail.com>
2023-06-30 23:26:32 +0700

    homework done

--- randomNumbers.js
+++ randomNumbers.js
@@ -1,3 +1,4 @@
+// Functions
 const generateRandomNumbers = (amount) => {
   let numbers = [];
 
@@ -6,22 +7,6 @@ const generateRandomNumbers = (amount) => {
   }
   return numbers;
 };
-
-const random = generateRandomNumbers(10);
-
-// const oddIndexNumbers = [];
-// const evenIndexNumbers = [];
-
-// for (const key in random) {
-//   console.log(key);
-//   if (key % 2 === 1) {
-//     oddIndexNumbers.push(random[key]);
-//   } else if (key % 2 === 0) {
-//     evenIndexNumbers.push(random[key]);
-//   }
-// }
-// console.log(oddIndexNumbers);
-// console.log(evenIndexNumbers);
 
 const getArrayByIndex = (oddOrEven, fromArray) => {
   let choice = oddOrEven.toLowerCase();
@@ -65,20 +50,6 @@ const getMaxOrMin = (maxOrMin, fromArray) => {
   }
 };
 
-console.log(random);
-const oddIndexNumbers = getArrayByIndex("odd", random);
-const evenIndexNumbers = getArrayByIndex("even", random);
-const maxOddIndexNumbers = getMaxOrMin("max", oddIndexNumbers);
-const maxEvenIndexNumbers = getMaxOrMin("max", evenIndexNumbers);
-const minOddIndexNumbers = getMaxOrMin("min", oddIndexNumbers);
-const minEvenIndexNumbers = getMaxOrMin("min", evenIndexNumbers);
-console.log(oddIndexNumbers);
-console.log(evenIndexNumbers);
-console.log(maxOddIndexNumbers);
-console.log(minOddIndexNumbers);
-console.log(maxEvenIndexNumbers);
-console.log(minEvenIndexNumbers);
-
 const sumArray = (array) => {
   let sum = 0;
 
@@ -97,42 +68,78 @@ const averageArray = (array) => {
   return sum / array.length;
 };
 
-const sumEven = sumArray(evenIndexNumbers);
-const avgEven = averageArray(evenIndexNumbers);
-const sumOdd = sumArray(oddIndexNumbers);
-const avgOdd = averageArray(oddIndexNumbers);
-console.log(sumEven);
-console.log(avgEven);
-console.log(sumOdd);
-console.log(avgOdd);
-
 const compareMax = (maxOdd, maxEven) => {
-  if (maxOdd > maxEven) return `Nilai maksimum lebih besar array ganjil ${maxOdd} vs ${maxEven}`;
-  if (maxOdd < maxEven) return `Nilai maksimum lebih besar array genap`;
-  if (maxOdd === maxEven) return `Nilai maksimum kedua array sama`;
+  if (maxOdd > maxEven) return `Nilai Max: array ganjil > array genap (${maxOdd} vs ${maxEven})`;
+  if (maxOdd < maxEven) return `Nilai Max: array ganjil < array genap (${maxOdd} vs ${maxEven})`;
+  if (maxOdd === maxEven) return `Nilai Max: array ganjil = array genap (${maxOdd} vs ${maxEven})`;
 };
 
-console.log(compareMax(maxOddIndexNumbers, maxEvenIndexNumbers));
-
 const compareMin = (minOdd, minEven) => {
-  if (minOdd > minEven) return `Nilai minimum lebih besar array ganjil ${minOdd} vs ${minEven}`;
-  if (minOdd < minEven) return `Nilai minimum lebih besar array genap`;
-  if (minOdd === minEven) return `Nilai minimum kedua array sama`;
+  if (minOdd > minEven) return `Nilai Min: array ganjil > array genap (${minOdd} vs ${minEven})`;
+  if (minOdd < minEven) return `Nilai Min: array ganjil < array genap (${minOdd} vs ${minEven})`;
+  if (minOdd === minEven) return `Nilai Min: array ganjil = array genap (${minOdd} vs ${minEven})`;
 };
 
 const compareSum = (sumOdd, sumEven) => {
-  if (sumOdd > sumEven) return `Nilai total lebih besar array ganjil ${sumOdd} vs ${sumEven}`;
-  if (sumOdd < sumEven) return `Nilai total lebih besar array genap`;
-  if (sumOdd === sumEven) return `Nilai total kedua array sama`;
+  if (sumOdd > sumEven)
+    return `Nilai total penjumlahan: array ganjil > array genap (${sumOdd} vs ${sumEven})`;
+  if (sumOdd < sumEven)
+    return `Nilai total penjumlahan: array ganjil < array genap (${sumOdd} vs ${sumEven})`;
+  if (sumOdd === sumEven)
+    return `Nilai total penjumlahan: array ganjil = array genap (${sumOdd} vs ${sumEven})`;
 };
 
 const compareAvg = (avgOdd, avgEven) => {
-  if (avgOdd > avgEven) return `Nilai rata-rata lebih besar array ganjil ${avgOdd} vs ${avgEven}`;
-  if (avgOdd < avgEven) return `Nilai rata-rata lebih besar array genap`;
-  if (avgOdd === avgEven) return `Nilai rata-rata kedua array sama`;
+  if (avgOdd > avgEven)
+    return `Nilai rata-rata: array ganjil > array genap (${avgOdd} vs ${avgEven})`;
+  if (avgOdd < avgEven)
+    return `Nilai rata-rata: array ganjil < array genap (${avgOdd} vs ${avgEven})`;
+  if (avgOdd === avgEven)
+    return `Nilai rata-rata: array ganjil = array genap (${avgOdd} vs ${avgEven})`;
 };
+//Functions END
 
-console.log(compareMin(minOddIndexNumbers, minEvenIndexNumbers));
-console.log(compareMax(maxOddIndexNumbers, maxEvenIndexNumbers));
+// Penggunaan Functions
+const random = generateRandomNumbers(100);
+console.log(`Angka acak yang didapatkan adalah: ${random.join(", ")}`);
+
+// Array index ganjil
+console.log(`\nArray dengan index ganjil:`);
+const oddIndexNumbers = getArrayByIndex("odd", random);
+console.log(`Berisi: ${oddIndexNumbers.join(", ")}`);
+
+const maxOdd = getMaxOrMin("max", oddIndexNumbers);
+console.log(`Nilai Max: ${maxOdd}`);
+
+const minOdd = getMaxOrMin("min", oddIndexNumbers);
+console.log(`Nilai Min: ${minOdd}`);
+
+const sumOdd = sumArray(oddIndexNumbers);
+console.log(`Total penjumlahan: ${sumOdd}`);
+
+const avgOdd = averageArray(oddIndexNumbers);
+console.log(`Rata-rata: ${avgOdd}`);
+
+//Array index genap
+console.log(`\nArray dengan index genap:`);
+const evenIndexNumbers = getArrayByIndex("even", random);
+console.log(`Berisi: ${evenIndexNumbers.join(", ")}`);
+
+const maxEven = getMaxOrMin("max", evenIndexNumbers);
+console.log(`Nilai Max: ${maxEven}`);
+
+const minEven = getMaxOrMin("min", evenIndexNumbers);
+console.log(`Nilai Min: ${minEven}`);
+
+const sumEven = sumArray(evenIndexNumbers);
+console.log(`Total penjumlahan: ${sumEven}`);
+
+const avgEven = averageArray(evenIndexNumbers);
+console.log(`Rata-rata: ${avgEven}`);
+
+//Perbandingan array
+console.log(`\nPerbandingan array ganjil vs genap:`);
+console.log(compareMax(maxOdd, maxEven));
+console.log(compareMin(minOdd, minEven));
 console.log(compareSum(sumOdd, sumEven));
 console.log(compareAvg(avgOdd, avgEven));
